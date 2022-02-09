1639eb28dc6b09b7c2d87d7f6106779137861999
Washington Sampaio Vieira <washingtonsampaio.23@gmail.com>
2022-02-09 17:36:30 -0300

    Iniciando projeto, base criada no spring.io

---
+++ src/main/java/br/com/serasa/vendedores/VendedoresApplication.java
@@ -0,0 +1,13 @@
+package br.com.serasa.vendedores;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VendedoresApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VendedoresApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/serasa/vendedores/VendedoresApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.serasa.vendedores;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VendedoresApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
